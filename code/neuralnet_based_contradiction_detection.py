d278eca17491485f2a0b818de6baaab62898f331
Diana Mincu <diana.p.mincu@gmail.com>
2016-05-30 16:32:53 +0300

    added code to train neural net model in keras

--- code/neuralnet_based_contradiction_detection.py
+++ code/neuralnet_based_contradiction_detection.py
@@ -8,8 +8,8 @@ import sys
 import time
 
 from gensim.models import word2vec
-#from keras.layers import merge, Convolution2D, MaxPooling2D, Input, Dense, Flatten
-#from keras.models import Model
+from keras.layers import merge, Convolution2D, MaxPooling2D, Input, Dense, Flatten
+from keras.models import Model
 
 
 
